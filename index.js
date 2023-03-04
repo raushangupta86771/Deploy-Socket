e9e64a0b33f5bef868bdb00d555fa12a442bda2b
raushangupta86771 <raushandsa@gmail.com>
2023-03-05 01:24:33 +0530

    link added

--- index.js
+++ index.js
@@ -1,51 +1,72 @@
-const port = "https://socket-ecommerce.onrender.com" || 8900;
-// console.log(process.env.ORIGIN_REACT_APP)
-const io = require('socket.io')(port, {
-    cors: {
-        origin: "https://funny-gecko-10a63a.netlify.app" || "http://localhost:3000"
-    },
-})
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
-let users = []
+const app = express();
+const server = http.createServer(app);
+
+const port = process.env.PORT || 8900;
+
+// create socket io instance
+const io = socketIo(server, {
+  cors: {
+    origin: ["https://funny-gecko-10a63a.netlify.app", "http://localhost:3000"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
+
+let users = [];
 
 const addUser = (userId, socketId) => {
-    //if userId is there in users[] then keep it else add it
-    !users.some((user) => user.userId === userId) &&
-        users.push({ userId, socketId })
-}
+  //if userId is there in users[] then keep it else add it
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
 
 const removeUser = (socketId) => {
-    users = users.filter(user => user.socketId !== socketId)
-}
+  users = users.filter((user) => user.socketId !== socketId);
+};
 
 const getUser = (receiverId) => {
-    return users.find((user) => user.userId === receiverId)
-}
+  return users.find((user) => user.userId === receiverId);
+};
 
+// listen for connection event
 io.on("connection", (socket) => {
-    
-    //when user connected
-    console.log("a user connected")
-    // io.emit("welcome","hello this is socket server")
-    //take userId and socketId from user 
-    socket.on("addUser", (userId) => {
-        addUser(userId, socket.id)
-        io.emit("getUsers", users);
-    })
+  console.log(`New client connected: ${socket.id}`);
 
-    //send and get message
-    socket.on("sendMessage",({senderId,receiverId, text})=>{
-        const user = getUser(receiverId);
-        console.log("hello")
-        io.to(user?.socketId).emit("getMessage",{
-            senderId,text
-        })
-    })
+  // listen for addUser event
+  socket.on("addUser", (userId) => {
+    console.log(`User ${userId} connected with socket id ${socket.id}`);
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
 
-    //when user disconnected
-    socket.on("disconnect", () => {
-        console.log("a user is disconnected");
-        removeUser(socket.id);
-        io.emit("getUsers", users);
-    })
-})
+  // listen for sendMessage event
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    if (!user) {
+      console.log(`User ${receiverId} is not connected to the socket`);
+      return;
+    }
+
+    console.log(`Sending message from ${senderId} to ${receiverId}: ${text}`);
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text
+    });
+  });
+
+  // listen for disconnection event
+  socket.on("disconnect", () => {
+    console.log(`Client disconnected: ${socket.id}`);
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Socket server running on port ${port}`);
+});
